d8550beb46ad7cdf623dd084bbdec0960faa6cf6
Kai Davenport <kaiyadavenport@gmail.com>
2014-07-13 14:19:33 +0000

    tests for decimals

--- test.js
+++ test.js
@@ -11,6 +11,9 @@ tape('convert numbers', function(t){
 	t.equal(percent(.2, 1), '20%', '20% from 1')
 	t.equal(percent(.2, .8), '25%', '25% from 1/4')
 
+	t.equal(percent(3.2), '3.2%', 'hold decimals')
+	t.equal(percent(3.249), '3.25%', 'hold decimals to 2dp')
+
 
 	t.equal(percent(), '0%', 'zero value')
 
